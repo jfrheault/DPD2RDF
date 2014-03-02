6a5af9f60ad8e3aae4dc7afa40286f83b9de1838
JFRheault <jf.rheault.1@gmail.com>
2014-03-02 16:42:12 -0500

    Apk done
    Signed-off-by: JFRheault <jf.rheault.1@gmail.com>

---
+++ semanticDPD/platforms/android/bin/HelloCordova-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/jfrheault/universite/bio2rdf/DPD2RDF/semanticDPD/platforms/android/bin/HelloCordova-release-unsigned.apk : \
+/home/jfrheault/universite/bio2rdf/DPD2RDF/semanticDPD/platforms/android/bin/HelloCordova.ap_ \
+/home/jfrheault/universite/bio2rdf/DPD2RDF/semanticDPD/platforms/android/bin/classes.dex \
+/home/jfrheault/universite/bio2rdf/DPD2RDF/semanticDPD/platforms/android/CordovaLib/bin/classes.jar \
